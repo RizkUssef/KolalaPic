620c8a1fbc32680e1143c17295ce4b1c2aee9cf7
Rizk Ussef <rizkusgt67@gmail.com>
2025-03-24 09:57:37 +0200

    create the logout api function and handle it

--- src/Controllers/apiLogoutController.php
+++ src/Controllers/apiLogoutController.php
@@ -3,9 +3,32 @@
 namespace Rizk\Kolala\Controllers;
 
 use Rizk\Kolala\Classes\Api;
+use Rizk\Kolala\Classes\Request;
+use Rizk\Kolala\Classes\Session;
+use Rizk\Kolala\Models\User;
 
 class apiLogoutController extends apiController{
     public function logout(){
-        
+        $api = new Api;
+        if(Request::checkGetExist("tkn")){
+            if(Session::checkSessionExist("user_token")){
+                $user_token = Request::get("tkn");
+                $user = new User;
+                $filter = ["user_token"=>$user_token];
+                $update = ['$set' =>["user_token"=>null]];
+                $user->update($filter,$update);
+                Session::removeSession("user_token");
+                http_response_code(200);
+                echo json_encode(["success"=>"you're logged out successfully"]);
+            }else{
+                http_response_code(404);
+                echo json_encode(["error"=>"you're already logged out"]);
+            }
+        }else{
+            http_response_code(404);
+            echo json_encode(["error"=>"unkown user"]);
+        }
+
+
     }
 }
