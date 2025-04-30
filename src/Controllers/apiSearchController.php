a1d25e7ec5e53940ca04b96af0acf406294d0d46
RizkUssef <rizkusgt67@gmail.com>
2025-05-01 00:59:32 +0300

    handle the search api function based on subcate & cate & auther

--- src/Controllers/apiSearchController.php
+++ src/Controllers/apiSearchController.php
@@ -2,11 +2,49 @@
 
 namespace Rizk\Kolala\Controllers;
 
-class apiSearchController{
-    public function __construct(){
+use Rizk\Kolala\Classes\Api;
+use Rizk\Kolala\Classes\Request;
+use Rizk\Kolala\Classes\Response;
+use Rizk\Kolala\Classes\Session;
+use Rizk\Kolala\Models\Photo;
+use Rizk\Kolala\Models\User;
+
+class apiSearchController
+{
+    public function __construct()
+    {
         session_start();
     }
-    public function SearchHandle(){
-        
+    public function SearchHandle()
+    {
+        $api = new Api;
+        $headers = getallheaders();
+        if (strtoupper(Request::Method()) === "POST") {
+            if (isset($headers["tkn"])) {
+                $tkn = $headers["tkn"];
+                if (Session::getSession("user_token") == $tkn) {
+                    $jsonInput = file_get_contents("php://input");
+                    $data = json_decode($jsonInput, true);
+                    $photo = new Photo;
+                    $filtered_text = strtolower($data["search_input"]);
+                    $photo_filter = [
+                        '$or' => [
+                            ["subcategory" => $filtered_text], 
+                            ["category" => $filtered_text], 
+                            ["auther" => $filtered_text]
+                        ]
+                    ];
+                    echo ($api->all(Photo::class, $photo_filter));
+
+                    // Response::msg(["success" => $photoDB], 200);
+                } else {
+                    Response::msg(["error" => "unAuth User"], 404);
+                }
+            } else {
+                Response::msg(["error" => "unknown User"], 404);
+            }
+        } else {
+            Response::msg(["error" => "access denied"], 404);
+        }
     }
 }
